8a59b04fb420527eb271e72ba135271a254c88eb
Aditya Pratap Singh <adisinghrajput@gmail.com>
2017-06-22 00:22:27 +0200

    Add structs with methods and static methods

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -5,6 +5,27 @@ struct User {
     active: bool
 }
 
+#[derive(Debug)]
+struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.length * self.width
+    }
+
+    fn can_hold(&self, rect2: &Rectangle) -> bool {
+        rect2.area() < self.area()
+    }
+
+    // static methods
+    fn square(size: u32) -> Rectangle {
+        Rectangle { length: size, width: size }
+    }
+}
+
 fn main() {
     let user = User {
         email: String::from("example@abc.com"),
@@ -12,6 +33,35 @@ fn main() {
         login_count: 0,
         active: true
     };
-    
-    
+
+    println!("User {}'s email is {}", user.name, user.email);
+
+    let rect1 = Rectangle { length: 50, width: 30 };
+    let rect2 = Rectangle { length: 40, width: 10 };
+    let rect3 = Rectangle { length: 45, width: 60 };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        &rect1.area()
+    );
+
+    println!(
+        "Can rect1 hold rect2? {}", 
+        rect1.can_hold(&rect2)
+    );
+
+    println!(
+        "Can rect1 hold rect3? {}", 
+        rect1.can_hold(&rect3)
+    );
+
+    println!(
+        "A square from rectangle is {:#?}", 
+        Rectangle::square(4)
+    );
+
+    println!(
+        "The area of the rectangle is {:#?} square pixels.",
+        rect1
+    );
 }
