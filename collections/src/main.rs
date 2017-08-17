f1ec497cd4bed7475400152bdf425c5948bfba40
Aditya Pratap Singh <adisinghrajput@gmail.com>
2017-08-18 00:11:38 +0200

    Add function to find mode of a list

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,26 +1,53 @@
 extern crate chrono;
-
-use chrono::{DateTime, Local, Utc};
-
-struct Stock {
-    open: f64,
-    close: f64,
-    high: f64,
-    low: f64,
-    date: DateTime<Utc>
-}
+use std::collections::HashMap;
 
 fn main() {
     println!("Hello, world!");
-
-    let mut stocks: Vec<Stock> = Vec::new();
-    let mut stock = Stock {
-        open: 1.2, 
-        close: 2.4,
-        high: 3.6,
-        low: 1.2,
-        date: Utc::now()
-    };
-
-    stocks.push(stock);
+    let list = vec![1,2,1,3,2,3,3,3,4,5,6,5,7,3,2,8,5,1,9];
+    
+    mode(list);
 }
+
+fn mode (list: Vec<i32>) {
+
+    let mut map: HashMap<i32, i32> = HashMap::new();
+
+    for num in list {
+        let count = map.entry(num).or_insert(0);
+        *count += 1;
+    }
+
+    let mut largest_count: i32 = 0;
+    let mut largest_count_num: i32 = 0;
+
+    for (num, count_num) in map {
+        if count_num > largest_count {
+            largest_count = count_num;
+            largest_count_num = num;
+        }
+    }
+
+    println!("The mode of the list is {} with count {}", largest_count_num, largest_count);
+}
+
+fn addEmployeeToOrg () {
+
+}
+
+// fn RLE () {
+
+//     // Read the data from the file
+//     let story = String::from("A list of characters to encode using RLE and basic Huffman encoding. This will just be to use encode encode and reduce size");
+
+//     // Create a map of words from word to character
+//     let mut map = HashMap::new();
+
+//     for word in story.split_whitespace() {
+//         let count = map.entry(word).or_insert(0);
+//         *count += 1;
+//     }
+
+//     // Then iterate through the words and replace the words with code
+
+//     // If the word gets repeated consecutively, add 1 to the count
+// }
